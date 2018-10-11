22b3e5c19900b3399b95de328dbef647b2d3d875
RayBowman2017b <RayBowman2017b@gmail.com>
2018-10-11 14:45:16 -0500

    2018-10-11 14-40 webpack dev-server not updating

--- z32_gitaddcmt.bat
+++ z32_gitaddcmt.bat
@@ -6,6 +6,10 @@ git commit -m "2018-10-11 14-40 webpack dev-server not updating"
 @rem git push Budget_app_03 master
 @rem 
 @rem git push origin master
+@rem     url = git@github.com:RayBowman2017b/react-course-2-Budget-app.git
+@rem https://github.com/RayBowman2017b/react-course-2-expensify-app
+@rem https://github.com/RayBowman2017b/react-course-2-Budget-app_03
+@rem https://github.com/RayBowman2017b/react-course-2-Budget-app_03b
 @rem git push Budget_app master
  git push Budget_app master
 @rem 
