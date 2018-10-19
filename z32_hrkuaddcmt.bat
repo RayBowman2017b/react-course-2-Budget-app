14e983075e6d548266fd6e2dadb51ce5b08a757b
RayBowman2017b <RayBowman2017b@gmail.com>
2018-10-18 20:29:07 -0500

    2018-10-18 20-31 SEC 015 L156

--- z32_hrkuaddcmt.bat
+++ z32_hrkuaddcmt.bat
@@ -2,7 +2,7 @@
 @rem 
 git add .
 @rem  git commit -m "2018-10-16 23-30 SEC 014"
-git commit -m "2018-10-18 20-21 SEC 015 L156"
+git commit -m "2018-10-18 20-31 SEC 015 L156"
 @rem git push Budget_app_03b master
 @rem git push Budget_app_03 master
 @rem 
